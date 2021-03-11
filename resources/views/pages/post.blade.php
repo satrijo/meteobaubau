099390e757017543f7c6c1551a1670b9a0324a78
Riyo Wicaksono <stryuw@gmail.com>
2021-03-11 21:24:50 +0800

    post layout2

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -6,18 +6,18 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col">
-                <article>
-                    @foreach($data as $d)
-                    <div class="article__title text-center">
-                        <h1 class="h2">{{$d->name}}</h1> <span>Category: {{$d->category->name}} </span><br />
-                        <span>Created: {{$d->created_at->format('d M Y')}} by {{$d->user->name}}</span>
-                    </div>
-                    <div class="boxed boxed-border">
-                        <img src="/storage/{{$d->image}}" height="300px" class="img--fullwidth" style="object-fit: cover; object-position: center;">
-                        {!! nl2br($d->text)!!}
-                    </div>
-                    @endforeach
-                </article>
+
+                @foreach($data as $d)
+                <div class="article__title text-center">
+                    <h1 class="h2">{{$d->name}}</h1> <span>Category: {{$d->category->name}} </span><br />
+                    <span>Created: {{$d->created_at->format('d M Y')}} by {{$d->user->name}}</span>
+                </div>
+                <div class="boxed boxed-border">
+                    <img src="/storage/{{$d->image}}" height="300px" class="img--fullwidth" style="object-fit: cover; object-position: center;">
+                    {!! nl2br($d->text)!!}
+                </div>
+                @endforeach
+
             </div>
         </div>
     </div>
